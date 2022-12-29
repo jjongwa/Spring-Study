3e0a201f69605cd5c300bc26026f8c38fb1b8c8b
jjongwa <troas96@naver.com>
2022-12-30 02:18:29 +0900

    회원 서비스 테스트 (코드 정리) by 회원 관리 예제 - 백엔드 개발

--- Intro/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ Intro/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -3,13 +3,11 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
     MemberService memberService;
