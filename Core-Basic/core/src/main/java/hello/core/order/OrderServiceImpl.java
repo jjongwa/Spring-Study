1e76fea6ee55c99439bd683a729e17097a7fd428
jjongwa <troas96@naver.com>
2023-02-01 16:37:26 +0900

    @AutoWired 필드 명, @Qualifier, @Primary 의존관계 자동 주입
    스프링 핵심 원리 - 기본편

--- Core-Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Core-Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,7 +18,7 @@ public class OrderServiceImpl implements OrderService {
     private final DiscountPolicy discountPolicy;
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository,  DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
